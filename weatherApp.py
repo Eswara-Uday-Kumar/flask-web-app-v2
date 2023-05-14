9c388ab110ffe8fb9d9924653ddf54b7e82cd87b
oevsuk <o.vijayalakshmi1977@gmail.com>
2023-05-14 12:33:07 +0530

    Update weatherApp.py

--- weatherApp.py
+++ weatherApp.py
@@ -1,4 +1,17 @@
 import json
 import requests
 
+token = "b6a83c4d78b1488bae863944231405"
+baseurl = "https://api.weatherapi.com/v1/"
+api = ""
+location = input("Enter city name: ").capitalize()
+days = int(input("Enter number of history days: "))
+
+if days == 1:
+    api = baseurl+"current.json?key="+token+"&q="+location+"&aqi=no"
+elif days > 1:
+    api = baseurl+"forecast.json?key="+token+"&q="+location+"&days="+str(days)+"&aqi=no&alerts=no"
+
+
+print(api)
 
