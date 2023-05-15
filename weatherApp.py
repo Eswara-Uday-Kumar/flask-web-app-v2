fc697393f6041cd46aae9245a588714243ce8e88
oevsuk <o.vijayalakshmi1977@gmail.com>
2023-05-15 14:20:49 +0530

    Update weatherApp.py

--- weatherApp.py
+++ weatherApp.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import pandas as pd
 
 token = "b6a83c4d78b1488bae863944231405"
 baseurl = "https://api.weatherapi.com/v1/"
@@ -20,4 +21,29 @@ response = res.json()
 # for one day #print("Temperature: ", response['current']['temp_c'], "Feels Like: ", response['current']['feelslike_c'])
 # for one day #print("Condition: ",response['current']['condition']['text'])
 
-print(response)
+#print(response)
+dates, max_temp, min_temp, avg_temp, conditions =  ([] for i in range(5))
+
+for i in range(0, days):
+    #print(response['forecast']['forecastday'][i]['date'], response['forecast']['forecastday'][i]['day']['maxtemp_c'], response['forecast']['forecastday'][i]['day']['mintemp_c'], response['forecast']['forecastday'][i]['day']['avgtemp_c'], response['forecast']['forecastday'][i]['day']['condition']['text'])
+    dates.append(response['forecast']['forecastday'][i]['date'])
+    max_temp.append(response['forecast']['forecastday'][i]['day']['maxtemp_c'])
+    min_temp.append(response['forecast']['forecastday'][i]['day']['mintemp_c'])
+    avg_temp.append(response['forecast']['forecastday'][i]['day']['avgtemp_c'])
+    conditions.append(response['forecast']['forecastday'][i]['day']['condition']['text'])
+
+
+data = {
+    'Date': dates,
+    'max_temp': max_temp,
+    'min_temp': min_temp,
+    'avg_temp': avg_temp,
+    'condition_txt': conditions
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+#for i in response['forecast']:
+#    print(i)
