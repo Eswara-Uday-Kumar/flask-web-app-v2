79f1a7b56265846c66b1766f2ddf35b93ebf7865
oevsuk <o.vijayalakshmi1977@gmail.com>
2023-05-15 14:32:19 +0530

    Update weatherApp.py

--- weatherApp.py
+++ weatherApp.py
@@ -2,17 +2,27 @@ import json
 import requests
 import pandas as pd
 
+#current = "provides weather details today"
+#forecast = "Number of days of weather forecast. Value ranges from 1 to 10"
+#history = "provides weather details for previous days"
+#Future = "provides weather details for Dates between 14 days and 300 days from today in the future in yyyy-MM-dd format"
+
+
 token = "b6a83c4d78b1488bae863944231405"
 baseurl = "https://api.weatherapi.com/v1/"
 api = ""
 location = input("Enter city name: ").capitalize()
-days = int(input("Enter number of history days: "))
+days = int(input("Enter number of days: "))
+choice = input("select options Current or Forecast or History: ")
 
-if days == 1:
+
+if choice.lower() == 'current':
     api = baseurl+"current.json?key="+token+"&q="+location+"&aqi=no"
-elif days > 1:
-    api = baseurl+"forecast.json?key="+token+"&q="+location+"&days="+str(days)+"&aqi=no&alerts=no"
-
+elif choice.lower() == 'forecast':
+    api = baseurl+"forecast.json?key="+token+"&q=" + \
+        location+"&days="+str(days)+"&aqi=no&alerts=no"
+elif choice.lower() == 'history':
+    pass
 
 print(api)
 
@@ -21,8 +31,8 @@ response = res.json()
 # for one day #print("Temperature: ", response['current']['temp_c'], "Feels Like: ", response['current']['feelslike_c'])
 # for one day #print("Condition: ",response['current']['condition']['text'])
 
-#print(response)
-dates, max_temp, min_temp, avg_temp, conditions =  ([] for i in range(5))
+# print(response)
+dates, max_temp, min_temp, avg_temp, conditions = ([] for i in range(5))
 
 for i in range(0, days):
     #print(response['forecast']['forecastday'][i]['date'], response['forecast']['forecastday'][i]['day']['maxtemp_c'], response['forecast']['forecastday'][i]['day']['mintemp_c'], response['forecast']['forecastday'][i]['day']['avgtemp_c'], response['forecast']['forecastday'][i]['day']['condition']['text'])
@@ -45,5 +55,5 @@ df = pd.DataFrame(data)
 
 print(df)
 
-#for i in response['forecast']:
+# for i in response['forecast']:
 #    print(i)
