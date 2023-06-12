10f54d5575d275d2aef718b606a68c72946a7236
oevsuk <o.vijayalakshmi1977@gmail.com>
2023-06-12 07:22:42 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,3 +2,11 @@ print("Testing Github commit")
 
 dates, max_temp, min_temp, avg_temp, conditions =  ([] for i in range(5))
 print(dates)
+
+js = {'source': 'msx', 'entity': 'dbo_mssales', 'version': 'v1'}
+print(type(js))
+string = ""
+for k,v in js.items():
+    string += '"' + k + '" = ' + "'" + v + "'" + "\n"
+
+print(string)
