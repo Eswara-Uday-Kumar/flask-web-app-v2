3002d403c636394a8b7f69234e0395b3c12bbe63
oevsuk <82275897+Eswara-Uday-Kumar@users.noreply.github.com>
2023-07-02 00:21:08 +0530

    Read jobs from cloud db

---
+++ SQL_Queries.sql
@@ -0,0 +1,16 @@
+show databases;
+
+use careersv2;
+
+SHOW Tables;
+
+CREATE TABLE jobs (
+	id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(250) NOT NULL,
+    location VARCHAR(250) NOT NULL,
+    salary INT,
+    currency VARCHAR(10),
+    responsibilities VARCHAR(2000),
+    requirements VARCHAR (2000),
+    PRIMARY KEY (id)
+) ;
