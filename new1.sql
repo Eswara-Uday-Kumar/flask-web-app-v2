4310137cbc44a30b3c864e86622e961756a428d2
oevsuk <82275897+Eswara-Uday-Kumar@users.noreply.github.com>
2023-07-02 01:16:08 +0530

    small Change

--- new1.sql
+++ new1.sql
@@ -2,11 +2,11 @@ INSERT INTO jobs (title, location) VALUES ('Data Analyst', 'Bangalore, India');
 
 select * from careersv2.jobs;
 
-UPDATE `careersv2`.`jobs` SET `responsibilities` = 'Translate designs and wireframes into high quality JS, CSS, HTML templates
-Design, build, and maintain high performance, reusable, and reliable UI components and products
-Ensure the best possible performance, quality, and optimize for maximum speed and scalability
-Identify and correct bottlenecks and fix bugs.
-Help maintain code quality, organization, and automatization', `requirements` = 'Strong knowledge of programming skills in JS, CSS and HTML
-Familiarity with responsive and adaptive web design, and good knowledge of JS libraries such as JQuery
-Strong knowledge of about atleast one of the JS frameworks (e.g. VueJS, Angular JS, NodeJS, ReactJS)
-Experience with building websites, ability to handle cross browser compatibility issues' WHERE (`id` = '3');
+UPDATE `careersv2`.`jobs` SET `responsibilities` = 'Design and develop a cloud based backend
+Participate and produce a scalable cloud based backend system
+Design and develop REST based APIs
+Interact with customer directly and with other stakeholders in the organization', `requirements` = 'Hands on experience with building a web backend in Java or Golang
+Knowledge of designing and building REST APIs
+Proven experience in building a scalable and resilient backend
+Good understanding of database schemas and using both relational (SQL) and noSQL based data stores
+Strong analytical and debugging skills' WHERE (`id` = '4');
