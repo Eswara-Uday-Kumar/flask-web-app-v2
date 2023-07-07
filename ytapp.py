c90d8dab50d05499f776098332a73d983cfdcc9a
oevsuk <82275897+Eswara-Uday-Kumar@users.noreply.github.com>
2023-07-07 19:39:09 +0530

    modified

--- ytapp.py
+++ ytapp.py
@@ -12,17 +12,20 @@ def Home():
 
 @ytapp.route("/download", methods = ["POST"])
 def download():
+    resolutions = ['240p', '360p', '480p', '720p', '1080p']
     if request.method == 'POST':
         result = request.form
         video_url = request.form['yturl']
-        resolution = request.form['resolution']
+        resolution_cnt = request.form['resolution']
+        resolution = resolutions[int(resolution_cnt)]
         def video_download(url):
-            video = YouTube(url)
-            yd = video.streams.get_by_resolution(resolution)
+            yt = YouTube(url)
+            yd = yt.streams.get_by_resolution(resolution)
             yd.download()
 
         video_download(video_url)
-        return render_template("result.html", result=result)
+        return render_template('result.html', result=result)
+
 
 
 if __name__ == "__main__":
